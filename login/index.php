5f010c151e88e82ff1680535a30b0935883726f4
sangtruong0317 <truonggioisang.tdc2019@gmail.com>
2021-12-08 09:04:18 +0700

    login

---
+++ login/index.php
@@ -0,0 +1,96 @@
+<?php 
+session_start();
+require "../Admin/config.php";
+require "../Admin/model/db.php";
+require "../Admin/model/user.php";
+
+$user = new User;
+
+?>
+
+
+
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js"></script>
+<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+<!------ Include the above in your HEAD tag ---------->
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Login Page</title>
+   <!--Made with love by Mutiullah Samim -->
+   
+	<!--Bootsrap 4 CDN-->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+    
+    <!--Fontawesome CDN-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+
+	<!--Custom styles-->
+	<link rel="stylesheet" type="text/css" href="styles.css">
+</head>
+<body>
+<div class="container">
+	<div class="d-flex justify-content-center h-100">
+		<div class="card">
+			<div class="card-header">
+				<h3>Sign In</h3>
+				<div class="d-flex justify-content-end social_icon">
+					<span><i class="fab fa-facebook-square"></i></span>
+					<span><i class="fab fa-google-plus-square"></i></span>
+					
+				</div>
+			</div>
+			<div class="card-body">
+				<form action="" method="post">
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-user"></i></span>
+						</div>
+						<input type="text" class="form-control" placeholder="username" name="UserName" required="">
+					</div>
+					<div class="input-group form-group">
+						<div class="input-group-prepend">
+							<span class="input-group-text"><i class="fas fa-key"></i></span>
+						</div>
+						<input type="password" class="form-control" placeholder="password" name="Password" required="">
+					</div>
+					<div class="row align-items-center remember">
+						<input type="checkbox">Remember Me
+					</div>
+					<div class="form-group">
+						<input type="submit" value="Login" name="login" value="Get Started" class="btn float-right login_btn">
+					</div>
+				</form>
+			</div>
+			<div class="card-footer">
+				<div class="d-flex justify-content-center links">
+					Don't have an account?<a href="../register/index.php">Sign Up</a>
+				</div>
+				<div class="d-flex justify-content-center">
+					<a href="#">Forgot your password?</a>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+</body>
+</html>
+<?php
+if (isset($_POST['login'])){
+	$user_name = $_POST['UserName'];
+	$user_password = $_POST['Password'];
+	if($user->login($user_name, $user_password)==1){
+		$_SESSION['admin_name']=$user_name;
+		echo "<script> alert('Xin chào $user_name');window.location.href='../Admin/index.php'</script>";
+	}
+	else if($user->login($user_name, $user_password)==0){
+		$_SESSION['customer_name']=$user_name;
+		echo "<script> alert('Xin chào khách hàng $user_name');window.location.href='../index.php'</script>";
+	}
+	else{
+		echo "<script> alert('Tài khoản không đúng !! Vui lòng thử lại');window.location.href='index.php'</script>";
+	}
+} 
+?>
